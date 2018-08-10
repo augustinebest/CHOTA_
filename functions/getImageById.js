2dda96bdddfe7c8024e9d71c6456e4eb5f36a75e
augustinebest <augustusbest00@gmail.com>
2018-08-10 17:59:33 +0100

    this file enables you to get user by ID

---
+++ functions/getImageById.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Interest = require('../Models/interest');
+
+const getImageById = (id) => {
+    Interest.findById(id)
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(404).json({err: 'Unable to obtain the ID'});
+    });
+}
+
+module.exports = getImageById;
