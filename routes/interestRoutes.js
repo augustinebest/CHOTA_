f5389fc83f70545a7f022f731c05eca09d5e4e8c
augustinebest <augustusbest00@gmail.com>
2018-08-10 11:03:04 +0100

    i have included the delete routes

--- routes/interestRoutes.js
+++ routes/interestRoutes.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../Models/User');
-const passport = require('passport');
+const checkAuth = require('../checkAuth/checkAuth');
+const interestController = require('../Controllers/interestControllers');
+const path = require('path');
+const multer = require('multer');
+// const upload = multer({ dest: 'uploads/' });
 
-// To check if the user is logged in
-const checkAuth = (req, res, next) => {
-    if(!req.user) {
-        console.log('You are not logged in');
-        res.redirect('/auth/profile');
-    } else {
-        next();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb) {
+        // cb(null, file.originalname);
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
-}
-
-router.get('/', checkAuth, (req, res) => {
-    res.send('Hello ' + req.user);
 });
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+} 
+
+const upload = multer({
+    storage: storage,
+    limits: {
+    fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+ });
+ 
+router.post('/', upload.single('image'), interestController.addInterest);
+router.get('/', interestController.getAllInterest);
+router.delete('/:id', interestController.deleteInterest);
 
 module.exports = router;
