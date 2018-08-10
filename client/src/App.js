aaa42ef3abfe01d5bed409c1b0a28b7007a46f73
unknown <obinnacodes@gmail.com>
2018-08-10 08:44:42 +0100

    Added package.json for Material Ui and sematic Ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
+        
       </div>
     );
   }
