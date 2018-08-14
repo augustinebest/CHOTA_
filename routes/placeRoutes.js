3d8f0e458d1c96d4766caaf71ec909d72a19ec5a
Chika Josephine <josephinechika94@gmail.com>
2018-08-14 15:30:32 +0100

    rmodified place router and added a post method for places

--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -1,3 +1,36 @@
 const express = require('express');
 const router = express.Router();
+// const checkAuth = require('../checkAuth/checkAuth');
+const placeController = require('../Controllers/placesControllers');
+const path = require('path');
+const multer = require('multer');
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/places/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+} 
+
+const upload = multer({
+    storage: storage,
+    limits: {
+    fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+ });
+ 
+router.post('/', upload.single('image'), placeController.addPlaces);
+
+
+module.exports = router;
