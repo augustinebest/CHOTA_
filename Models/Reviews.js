24cd7e9987d3ca8ea11ed887aff2d2ed8e2ec7bd
Chika Josephine <josephinechika94@gmail.com>
2018-08-14 15:20:54 +0100

    removed the ending comma at line 6

--- Models/Reviews.js
+++ Models/Reviews.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const reviewSchema = mongoose.Schema({
     name : { type: String, required: true },
     image : { type: String, required: true },
-    comment : { type: String, required: true },
+    comment : { type: String, required: true }
 });
 
 
