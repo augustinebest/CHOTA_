55c27926387ce9064e5d8e82f3c2ccad13edcabc
Chika Josephine <josephinechika94@gmail.com>
2018-08-14 15:27:56 +0100

    created add places constructor in placeController.js file

--- Controllers/placesControllers.js
+++ Controllers/placesControllers.js
@@ -1,6 +1,26 @@
 const Place = require('../Models/Places');
-const multer = require('multer');
-const fs = require('fs');
-const deleteImage = require('../functions/delete');
+// const multer = require('multer');
+// const fs = require('fs');
+// const deleteImage = require('../functions/delete');
+
+exports.addPlaces = (req, res, next) => {
+    const place = new Place({
+        name: req.body.name,
+        image: req.file.path,
+        description: req.body.description,
+        reviews: req.body.reviews,
+        ratings: req.body.ratings
+    });
+    place.save()
+    .then(result => {
+        res.send(result);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+};
+
+
 
 
