f92db928af69dc416d46d0495577e610b456946b
Chika Josephine <josephinechika94@gmail.com>
2018-08-15 17:36:12 +0100

    added another property "categoryId" in line 8

--- Models/Places.js
+++ Models/Places.js
@@ -5,6 +5,7 @@ const placeSchema = mongoose.Schema({
     image: [{ type: String }],
     description: { type: String },
     date: { type: Date, default: Date.now },
+    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category'},
     ratings: { type: Number },
     reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reviews' }]
     // mapview: '',
