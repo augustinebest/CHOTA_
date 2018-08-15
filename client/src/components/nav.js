45f0ce56dc2108544908019e69667c76f448b4ad
kidmeeno <arthurbosah@yahoo.com>
2018-08-15 09:20:12 +0100

    edited version of the navbar. added the following: a className navbarBorder, a button, and the image logo 'chota'

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem, Button, ButtonToolbar} from "react-bootstrap"
+import {Navbar, Nav, NavItem,} from "react-bootstrap";
+import './nav.css';
+import logo from './logo_invt.png'
 
 
 const nav = () => {
     return(
       <div>
-        <Navbar inverse collapseOnSelect>
+        <Navbar className='navBarBorder'>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="/">HOME</a>
+            <a href="/"> <img src={logo} alt='chota'/></a>
           </Navbar.Brand>
+          <button className='btn'> Login </button>
           <Navbar.Toggle />
         </Navbar.Header>
-        <Navbar.Collapse>
+        <Navbar.Collapse className=''>
           <Nav pullRight>
             <NavItem eventKey={1} href="/about">
              About
