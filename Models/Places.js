85c35eff321ee8232f7ed224f94f186a4905b057
Chika Josephine <josephinechika94@gmail.com>
2018-08-15 11:25:22 +0100

    added an array to image property

--- Models/Places.js
+++ Models/Places.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const placeSchema = mongoose.Schema({
     name: { type: String },
-    image: [],
-    image1: [],
-    image2: [],
-    image3: [],
+    image: [{ type: String }],
     description: { type: String },
     ratings: { type: Number },
     reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reviews' }]
