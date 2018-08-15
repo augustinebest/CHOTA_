a0d702896dc0f236c4804f78b98a1dc5d59e0cfe
kidmeeno <arthurbosah@yahoo.com>
2018-08-15 09:00:01 +0100

    romoved the footer component out of App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Router from './router';
-import Footer from './components/footer'
+
 
 class App extends Component {
   render() {
     return (
   <div className="App">
     <Router/>
-    <Footer/>
+   
   </div>
     );
   }
