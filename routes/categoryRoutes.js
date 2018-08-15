6287d0373d7f0adde5660d12c3a48cd8609d4cdd
Chika Josephine <josephinechika94@gmail.com>
2018-08-15 17:35:30 +0100

    added a get method for  category router

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const category = require('../Controllers/categoryControllers');
 
 router.post('/', category.addCategory);
+router.get('/', category.getAllCategories);
 
 module.exports = router;
