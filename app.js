05b49cc07d0ab4b105774391936db4e98683bdd6
augustinebest <augustusbest00@gmail.com>
2018-08-20 15:49:19 -0700

    i fixed my cors origin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
-        "Access-Conrol-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     if(req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
