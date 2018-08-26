f88eaadc7beb57ee4e150798c427b6ae7da5c6c3
augustinebest <augustusbest00@gmail.com>
2018-08-26 08:12:18 -0700

    still working on the upload function in cloudinary where more than one image can be upload

--- functions/cloudinary.js
+++ functions/cloudinary.js
@@ -14,6 +14,18 @@ exports.upload = function(file){
    })
 }
 
+exports.uploads = function(files){
+    files.forEach(element => {
+        var file = element;
+        return new Promise(resolve => {
+            cloudinary.uploader.upload(file, function(result){
+                resolve({url: result.url, Id: result.public_id});
+                // console.log(result);
+            }, {resource_type: "auto"})
+       })
+    });
+}
+
 exports.delete = function(publicId) {
     cloudinary.uploader.destroy(publicId, function(err, result) {
         if(err) {
