6f5d519f02281daf90b642a7694d3b7471fae61d
Chika Josephine <josephinechika94@gmail.com>
2018-08-27 14:35:20 +0100

    added cloudinary details

--- config/keys.js
+++ config/keys.js
@@ -13,5 +13,12 @@ module.exports = {
         clientID: '432976223876763',
         clientSecret: 'f47fe851330cbf0187d0a2836263010d'
     },
-    mongodb: 'mongodb://chotaapp:chota123@ds033484.mlab.com:33484/chota'
+    mongodb: 'mongodb://chotaapp:chota123@ds033484.mlab.com:33484/chota',
+
+    cloudinary: {
+        cloud_name: 'chotaapp',
+        api_key: '662324536894151',
+        api_secret: '-Ksmzou9SDOxTUBZxjqs5BeOw-k'
+    }
+    
 }
