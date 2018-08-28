1f4975a5101fe7ff25c7f79289bbda37efbd933d
augustinebest <augustusbest00@gmail.com>
2018-08-28 07:33:13 -0800

    i configured my nodemailer and added some little html and css

---
+++ functions/mailer.js
@@ -0,0 +1,21 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'djangelopoku@gmail.com',
+        pass: '0kp0ku0n'
+    }
+});
+
+exports.recoveryPassword = function(email,callback,pass){
+    let mailOptions = {
+        from: '"Chota"',
+        to: email,
+        subject: ' Chota Password Recovery',
+        html: `<center><h2> <p>You have requested a password reset,<br> please use the number below to login and reset your password.</p></h2><center>
+                <br><br><h3><span style="color:rgb(222, 226, 224); background-color:blueviolet; padding: 20px; border-radius: 20px;">
+                <a href='http://localhost:5000/auth/recoverpassword/${pass}'>Confirm your password</a></span></h3>`
+ };
+ transporter.sendMail(mailOptions, callback);
+}
