2acce4c0a02f31668da894368bddba0d7ea77d29
augustinebest <augustusbest00@gmail.com>
2018-08-29 12:14:54 -0800

    i added a view object property that determines the number of views a particular place have at an instance

--- Models/Places.js
+++ Models/Places.js
@@ -8,8 +8,8 @@ const placeSchema = mongoose.Schema({
     date: { type: Date, default: Date.now },
     categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category'},
     ratings: { type: Number },
-    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reviews' }]
-    // mapview: '',
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reviews' }],
+    view: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('Place', placeSchema);
