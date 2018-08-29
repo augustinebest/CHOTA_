563c4f7e63e8d121a52aefcad3413bf15d09a3b5
Chika Josephine <josephinechika94@gmail.com>
2018-08-29 16:50:03 +0100

    added to additional properties in line 5 and 6

--- Models/Categories.js
+++ Models/Categories.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const CategorySchema = mongoose.Schema({
     categoryName: {type: String, unique: true},
+    image: { type: String },
+    imageID: { type: String },
     placeId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Place' }]
 });
 
