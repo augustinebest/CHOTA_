884c4d404dffa8b18130135a573bcaa6e828407f
Chika Josephine <josephinechika94@gmail.com>
2018-08-30 11:42:09 +0100

    modified cloudinary configuration constructor and required the path

--- functions/cloudinary.js
+++ functions/cloudinary.js
@@ -1,17 +1,19 @@
 // require('dotenv').config({path: '/config/secret'});
 const cloudinary = require('cloudinary');
+const keys = require('../config/keys');
+
 // const key = process.env.cloud_name;
 // const key1 = process.env.api_key;
 // const key2 = process.env.api_secret;
 
-// cloudinary.config({
-    // cloud_name = "chota"
-    // api_key = "454241471689352"
-    // api_secret = "pCWnCq-cSSiM4UEcBrnI2EGTguQ"
-//     cloud_name: keys.cloudinary.cloud_name,
-//     api_key: keys.cloudinary.api_key,
-//     api_secret: keys.cloudinary.api_secret
-//   })
+cloudinary.config({
+    // cloud_name = 'chota',
+    // api_key = '454241471689352',
+    // api_secret = 'pCWnCq-cSSiM4UEcBrnI2EGTguQ'
+    cloud_name: keys.cloudinary.cloudName,
+    api_key: keys.cloudinary.apiKey,
+    api_secret: keys.cloudinary.apiSecret
+  })
 
 exports.upload = function(file){
     return new Promise(resolve => {
