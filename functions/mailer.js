19f09ec0dd159575ed98360bebd0ba9947bd358d
augustinebest <augustusbest00@gmail.com>
2018-08-31 05:50:34 -0800

    i redesigned the html in the email

--- functions/mailer.js
+++ functions/mailer.js
@@ -16,7 +16,7 @@ exports.recoveryPassword = function(email,callback,pass){
         html: `<center><h2> <p>You have requested a password reset,<br> please use the number below to login and reset your password.</p></h2><center>
                 <br><br><h3><span style="color:rgba(4, 5, 17, 1); background-color:rgba(141, 173, 231, 1); padding: 20px; 
                 border-radius: 20px; font-weight: bold; border-radius: 5px; border: 1px solid rgba(4, 5, 17, 1)">
-                <a href='http://localhost:5000/auth/recoverpassword/${pass}'>Confirm your password</a></span></h3>`
+                <a href='http://localhost:5000/auth/recover?email=${email}&token=${pass}'>Confirm your password</a></span></h3>`
  };
  transporter.sendMail(mailOptions, callback);
 }
